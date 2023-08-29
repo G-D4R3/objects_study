6c95786e8878e88d11f06e5831f4f9aea1ded3ab
G-D4R3 <gdare1999@gmail.com>
2023-08-30 00:10:13 +0900

    first commit

---
+++ src/main/java/dare/dare/object_study/ObjectStudyApplication.java
@@ -0,0 +1,13 @@
+package dare.dare.object_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dare/dare/object_study/ObjectStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package dare.dare.object_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
